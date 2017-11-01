b67a53cb7ec0d87c2659cd7cc412878a1087376a
SizimaHaruka <hiems.via@gmail.com>
2017-11-02 03:15:52 +0900

    highlight test

--- wp-content/themes/personalio-child/functions.php
+++ wp-content/themes/personalio-child/functions.php
@@ -1,8 +1,18 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles');
+add_action('wp_enqueue_scripts', 'append_highlight_js');
+
 function theme_enqueue_styles() {
   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-  wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style')
-);
+  wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
 }
+function append_highlight_js() {
+    // スクリプトの追加
+    wp_enqueue_script('highlightjs', '//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.2/highlight.min.js');
+    wp_enqueue_script('highlightjsload', get_stylesheet_directory_uri() . '/highlightjs_call.js');
+
+    // スタイルの追加
+    wp_enqueue_style('highlightjs', '//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.2/styles/solarized_dark.min.css');
+}
+
 ?>
